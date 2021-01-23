01b1ed44b5921da10b059856688080ea1beb77f8
omayma ahmad <omayma.ahmad25@gmail.com>
2021-01-23 18:23:24 +0000

    set up the html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const path = require('path')
 const router = express.Router()
-
-router.get('/', (req, res) => {
-  
+// getting the notes.html file and presenting back 
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"))
+})
+// getting any url that isnt notes they are given the index file
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
 module.exports = router; 
