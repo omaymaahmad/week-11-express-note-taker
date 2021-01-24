f2a3d7e7c280d723971b7b5cfb40b8bdb93cb01e
omayma ahmad <omayma.ahmad25@gmail.com>
2021-01-24 14:51:10 +0000

    add note function to database

--- db/database.js
+++ db/database.js
@@ -2,16 +2,40 @@ const fs = require("fs")
 const util = require("util"); 
 
 const promisifyReadFile = util.promisify(fs.readFile);
+const promisifyWriteFile = util.promisify(fs.writeFile);
+
 
 class databaseFunctionality {
 
+    read() {
+        return promisifyReadFile("db/db.json", "utf8");
+    }
+
+    write(myNotes) {
+        return promisifyWriteFile("db/db.json", JSON.stringify(myNotes));
+    }
+
     getNotes(){
-            const dbContents =promisifyReadFile("db/db.json");
-            console.log(dbContents)
+        return this.read().then((myNotes) => {
+            let parsedNotes;
+        })
     }
 
 
-    addNote(){}
+    addNote(myNotes){
+       const { title, text } = myNotes;
+       
+       if (!title || !text) {
+           throw new Error("the 'title and 'text' cannot be blank");
+       }
+
+       const newNote = { title, text, id: util() };
+
+       return this.getNotes()
+       .then((myNotes) => [...myNotes, newNote])
+       .then((updatedNotes) => this.write(updatedNotes))
+       .then(() => newNote);
+    }
 
 
     deleteNote(){}
